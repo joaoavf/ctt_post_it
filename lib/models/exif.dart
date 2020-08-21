f01bb6fc702570609d730f4f916a1ec9a9dd18b1
joaoavf <joao.avf@gmail.com>
2020-08-20 23:10:18 -0300

    [NEW]: Creating a new EXIF Model and transforming data from Buscode object int o EXIF bytes.

--- lib/models/exif.dart
+++ lib/models/exif.dart
@@ -1,52 +1,69 @@
 import 'dart:convert';
 import 'dart:typed_data';
-import 'package:post_it/data/exif_sample.dart';
-import 'package:post_it/models/buscode.dart';
-import 'package:post_it/models/buscode_view.dart';
+
+import 'package:camera_tutorial/data/exif_sample.dart';
+import 'package:camera_tutorial/models/buscode.dart';
 import 'package:flutter/material.dart';
-import 'package:image/image.dart' as imglib;
 
 class Exif {
   final List<Uint8List> bytes = sampleExif;
   final AsciiCodec codec = AsciiCodec();
 
   Exif({@required Buscode buscode}) {
-    buscodeExifMap.forEach((key, value) => setBytes(
-        string: buscode[key], start: value['start'], length: value['length']));
+    setFormatId(buscode.formatId);
+    setIssuerCode(buscode.issuerCode);
+    setEquipmentId(buscode.equipmentId);
+    setBuscodeDate(buscode.buscodeDate);
+    setPhotoDate(buscode.photoDate);
+    setSerialNumber(buscode.serialNumber);
+    setItemPriority(buscode.itemPriority);
+    setTrackingIndicator(buscode.trackingIndicator);
   }
 
-  void setBytes({String string, int start, int length}) {
-    assert(string.length == length);
+  void setBytes({String string, int start}) {
     List<int> numbers = codec.encode(string);
-    for (var i = start; i < start + length; i++) {
+    for (var i = start; i < start + numbers.length; i++) {
       bytes[0][i] = numbers[i - start];
     }
   }
-}
 
-BuscodeView readExifFile(imglib.Image image) {
-  List<Uint8List> bytes = image.exif.rawData;
-  String decoded;
-  Map newMap = {};
-  buscodeExifMap.forEach((key, value) {
-    decoded =
-        readBytes(bytes: bytes, start: value['start'], length: value['length']);
-    newMap[key] = decoded;
-  });
-  BuscodeView buscodeCard;
-  buscodeCard = BuscodeView(
-      image: image,
-      buscodeDate: newMap['buscodeDate'],
-      equipmentId: newMap['equipmentId'],
-      issuerCode: newMap['issuerCode'],
-      formatId: newMap['formatId'],
-      itemPriority: newMap['itemPriority'],
-      serialNumber: newMap['serialNumber'],
-      trackingIndicator: newMap['trackingIndicator']);
-  return buscodeCard;
-}
+  void setFormatId(String formatId) {
+    assert(formatId.length == 4);
+    setBytes(string: formatId, start: 42);
+  }
 
-readBytes({List<Uint8List> bytes, int start, int length}) {
-  final AsciiCodec codec = AsciiCodec();
-  return codec.decode(bytes[0].sublist(start, start + length));
+  void setIssuerCode(String issuerCode) {
+    assert(issuerCode.length == 3);
+    setBytes(string: issuerCode, start: 102);
+  }
+
+  void setTrackingIndicator(String trackingIndicator) {
+    assert(trackingIndicator.length == 1);
+    setBytes(string: trackingIndicator, start: 126);
+  }
+
+  void setEquipmentId(String equipmentId) {
+    assert(equipmentId.length == 3);
+    setBytes(string: equipmentId, start: 79);
+  }
+
+  void setBuscodeDate(String buscodeDate) {
+    assert(buscodeDate.length == 19);
+    setBytes(string: buscodeDate, start: 150);
+  }
+
+  void setPhotoDate(String photoDate) {
+    assert(photoDate.length == 19);
+    setBytes(string: photoDate, start: 130);
+  }
+
+  void setItemPriority(String itemPriority) {
+    assert(itemPriority.length == 1);
+    setBytes(string: itemPriority, start: 90);
+  }
+
+  void setSerialNumber(String serialNumber) {
+    assert(serialNumber.length == 5);
+    setBytes(string: serialNumber, start: 170);
+  }
 }
